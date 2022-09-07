867eedc3c2e40ce2518dc99e8469cfd8c8362940
AleksandrukZakhar <zakharaleksandrukdev@gmail.com>
2022-09-07 20:14:56 +0300

    Add weather request

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const KEY = "dfa0fc71cd912959d3815771c5d574d1";
 
 const getWeather = async (url) => {
+    const req = new Request(url);
+
     try {
-        const response = await fetch(url);
+        const response = await fetch(req);
 
         if (response.status === 200) {
             const res = await response.json();
@@ -14,3 +16,7 @@ const getWeather = async (url) => {
         throw new Error(err);
     }
 };
+
+getWeather(
+    `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${KEY}`
+);
